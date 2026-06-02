bf17a64de3817240ef03290f2e91887dfde3207b
obi1105 <obi1105@e-corporation.co.jp>
2026-06-02 13:59:40 +0900

    servlet-jsp-kadaiをプッシュ

---
+++ kadai_link_tag/WEB-INF/src/sj/LinkServlet.java
@@ -0,0 +1,29 @@
+package sj;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LinkServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			
+		 	request.setCharacterEncoding("UTF-8");
+	        response.setContentType("text/html; charset=UTF-8");
+			request.getParameter("name");
+		
+			request.setAttribute("name", "侍太郎");
+	
+			RequestDispatcher forward = request.getRequestDispatcher("index.jsp");
+			forward.forward(request, response);
+			
+		}
+	}
+
+
